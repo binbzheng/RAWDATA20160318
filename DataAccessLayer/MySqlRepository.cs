9df84645f3258c75a9c0c587686c6caeab88e999
binbinzheng <zhengbinbin1115@gmail.com>
2016-03-18 12:08:57 +0100

    Rldlæa

---
+++ DataAccessLayer/MySqlRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DomainModel;
+
+namespace DataAccessLayer
+{
+    public class MySqlRepository : IRepository
+    {
+
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return  new List<Order>
+            {
+                new Order {Id = 1 },
+                new Order {Id = 2 },
+                new Order {Id = 3 },
+                new Order {Id = 4 },
+                new Order {Id = 5 }
+            };
+        }
+    }
+}
--- Web/Controllers/OrdersController.cs
+++ Web/Controllers/OrdersController.cs
@@ -4,20 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DataAccessLayer;
 
 namespace Web.Controllers
 {
     public class OrdersController : ApiController
     {
+        private readonly IRepository _repository = new MySqlRepository();
 
         public IHttpActionResult Get()
         {
-            var data = new
-            {
-                Name = "David",
-                Age = 21
-            };
-
+            var data = _repository.GetOrders();
             return Ok(data);
         }
 
